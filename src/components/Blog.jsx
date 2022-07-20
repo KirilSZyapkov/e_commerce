49e149f2ef8c0c766d8e7c21cb288385551dc9ff
Kiril Zyapkov <ziapkov@gmail.com>
2022-07-20 23:59:02 +0300

    Styling blog page.

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -21,57 +21,57 @@ function Blog() {
             <div className="editorialtext">
 
         
-                <article className="EditorialText-module--editorialText__AX95r EditorialText-module--paddingSmall__2x6Pv EditorialText-module--layoutMiddle__1Jih5">
-                    <div className="EditorialText-module--container__310k3">
-                        <h2 className="Heading-module--general__3HQET EditorialText-module--heading__Ah5A- Heading-module--large__3Esej">Някои постижения от миналата година, с които се гордеем.</h2>
-                        <div className="EditorialText-module--text__PPXwp">
-                            <div className="EditorialParagraph-module--general__2DBSm EditorialText-module--preamble__3Cc-Z">
+                <article className="blog-article">
+                    <div className="blog-article-container">
+                        <h2 className="blog-article-main-title">Някои постижения от миналата година, с които се гордеем.</h2>
+                        <div className="blog-article-content">
+                            <div className="blog-article-themes">
                                 <p>Да, знаем, че никой не харесва самохвалковци. Но в случай че искате да научите какви награди и признания получихме наскоро, ще ги изброим тук за вас. Няма нужда да използвате Google. Благодарим!</p>
                             </div>
                         
-                            <h21 className="Heading-module--general__3HQET EditorialText-module--subheading__19LPD Heading-module--small__SFfSh">Индекс за устойчивост на Dow Jones </h21>
+                            <h4 className="blog-article-title">Индекс за устойчивост на Dow Jones </h4>
                             
-                            <div className="EditorialParagraph-module--general__2DBSm EditorialText-module--paragraph__1mO8l">
-                                <p>За осма поредна година H&amp;M е включена в <a href="https://www.spglobal.com/esg/performance/indices/djsi-index-family">световния индекс на Dow Jones</a>. През 2020 г. бяхме четвърти с резултат от 70/100. Освен това бяхме представени в европейския индекс на Dow Jones с най-високия възможен резултат за зачитане правата на човека, докладване за опазване на околната среда, социално докладване и същественост.&nbsp; Постигнахме и най-високия резултат в нашата индустрия за управление на веригите за доставки и стратегия за нововъзникващи пазари.</p>
+                            <div className="blog-article-themes">
+                                <p>За осма поредна година SweetPick е включена в световния индекс на Dow Jones. През 2020 г. бяхме четвърти с резултат от 70/100. Освен това бяхме представени в европейския индекс на Dow Jones с най-високия възможен резултат за зачитане правата на човека, докладване за опазване на околната среда, социално докладване и същественост.&nbsp; Постигнахме и най-високия резултат в нашата индустрия за управление на веригите за доставки и стратегия за нововъзникващи пазари.</p>
                             </div>
                             
-                            <h21 className="Heading-module--general__3HQET EditorialText-module--subheading__19LPD Heading-module--small__SFfSh">FTSE4Good</h21>
+                            <h4 className="blog-article-title">FTSE4Good</h4>
                             
-                            <div className="EditorialParagraph-module--general__2DBSm EditorialText-module--paragraph__1mO8l">
-                                <p>H&amp;M вече отговаря на условията да бъде част от <a href="https://www.ftserussell.com/products/indices/ftse4good"> индекс серията на FTSE4Good</a>. Това означава, че сме в състояние да демонстрираме и измерим сериозни природни, социални и управленски стандарти на база независими оценки.</p>
+                            <div className="blog-article-themes">
+                                <p>SweetPick вече отговаря на условията да бъде част от  индекс серията на FTSE4Good. Това означава, че сме в състояние да демонстрираме и измерим сериозни природни, социални и управленски стандарти на база независими оценки.</p>
                             </div>
                             
-                            <h21 className="Heading-module--general__3HQET EditorialText-module--subheading__19LPD Heading-module--small__SFfSh">Индекс за прозрачност в областта на модата </h21>
+                            <h4 className="blog-article-title">Индекс за прозрачност в областта на модата </h4>
                             
-                            <div className="EditorialParagraph-module--general__2DBSm EditorialText-module--paragraph__1mO8l">
-                                <p>H&amp;M зае второ място в индекса за 2021 г. <a href="https://www.fashionrevolution.org/about/transparency/">за прозрачна мода на Fashion Revolution</a> с резултат от 68%. С този индекс Fashion Revolution оценява и класира 250 глобални модни марки на база това колко информация споделят за доставчиците си, за своите политики и практики, свързани с веригата за доставки, както и за социалното и екологичното си въздействие.</p>
+                            <div className="blog-article-themes">
+                                <p>SweetPick зае второ място в индекса за 2021 г. за прозрачна мода на Fashion Revolution с резултат от 68%. С този индекс Fashion Revolution оценява и класира 250 глобални модни марки на база това колко информация споделят за доставчиците си, за своите политики и практики, свързани с веригата за доставки, както и за социалното и екологичното си въздействие.</p>
                             </div>
                             
-                            <h21 className="Heading-module--general__3HQET EditorialText-module--subheading__19LPD Heading-module--small__SFfSh">„А списък“ на CDP </h21>
+                            <h4 className="blog-article-title">„А списък“ на CDP </h4>
                             
-                            <div className="EditorialParagraph-module--general__2DBSm EditorialText-module--paragraph__1mO8l">
-                                <p><a href="https://www.cdp.net/en/companies/companies-scores">CDP</a>&nbsp; (Проект за разкриване на информация, свързана с въглерода) е организация с нестопанска цел, която награждава корпорации, борещи се срещу изменението на климата. В годишния си „А списък“ CDP включва компаниите-пионери по отношение на прозрачността и действията по опазване на околната среда. Ние станахме част от този престижен списък през 2020 г. благодарение на предприетите дейности за намаляване на емисиите, ограничаване на рисковете за климата и изграждане на бъдеще с нулеви въглеродни емисии.</p>
+                            <div className="blog-article-themes">
+                                <p>CDP&nbsp; (Проект за разкриване на информация, свързана с въглерода) е организация с нестопанска цел, която награждава корпорации, борещи се срещу изменението на климата. В годишния си „А списък“ CDP включва компаниите-пионери по отношение на прозрачността и действията по опазване на околната среда. Ние станахме част от този престижен списък през 2020 г. благодарение на предприетите дейности за намаляване на емисиите, ограничаване на рисковете за климата и изграждане на бъдеще с нулеви въглеродни емисии.</p>
                             </div>
                             
-                            <h21 className="Heading-module--general__3HQET EditorialText-module--subheading__19LPD Heading-module--small__SFfSh">Система за следене на компаниите на Oxfam Australia</h21>
+                            <h4 className="blog-article-title">Система за следене на компаниите на Oxfam Australia</h4>
                             
-                            <div className="EditorialParagraph-module--general__2DBSm EditorialText-module--paragraph__1mO8l">
-                                <p>Бяхме включени в <a href="https://whatshemakes.oxfam.org.au/company-tracker/">годишната система на Oxfam Australia за следене на</a> компании, посветени на изплащането на справедливи възнаграждения за труд на работниците, произвеждащи облекло, и на ограничаването на разходите за труд.</p>
+                            <div className="blog-article-themes">
+                                <p>Бяхме включени в годишната система на Oxfam Australia за следене на компании, посветени на изплащането на справедливи възнаграждения за труд на работниците, произвеждащи облекло, и на ограничаването на разходите за труд.</p>
                             </div>
                             
-                            <h21 className="Heading-module--general__3HQET EditorialText-module--subheading__19LPD Heading-module--small__SFfSh">Показател за влакна и материали в рамките на корпоративния текстилен обмен</h21>
+                            <h4 className="blog-article-title">Показател за влакна и материали в рамките на корпоративния текстилен обмен</h4>
                             
-                            <div className="EditorialParagraph-module--general__2DBSm EditorialText-module--paragraph__1mO8l">
-                                <p>H&amp;M беше призната за една от 16-те водещи компании от общо 173, анализирани в рамките на Индекса за промени в материалите (<a href="https://mci.textileexchange.org/change-index/">MCI</a>) за 2019 г. Този индекс е начин за измерване, проследяване и сравняване на напредъка на устойчивостта на компанията, свързан с влакната и материалите.&nbsp; &nbsp;</p>
+                            <div className="blog-article-themes">
+                                <p>SweetPick беше призната за една от 16-те водещи компании от общо 173, анализирани в рамките на Индекса за промени в материалите (MCI) за 2019 г. Този индекс е начин за измерване, проследяване и сравняване на напредъка на устойчивостта на компанията, свързан с влакната и материалите.&nbsp; &nbsp;</p>
                             </div>
                             
-                            <h21 className="Heading-module--general__3HQET EditorialText-module--subheading__19LPD Heading-module--small__SFfSh">Идеи на компаниите за бърза промяна на света 2021 г.</h21>
+                            <h4 className="blog-article-title">Идеи на компаниите за бърза промяна на света 2021 г.</h4>
                             
-                            <div className="EditorialParagraph-module--general__2DBSm EditorialText-module--paragraph__1mO8l">
-                                <p>Наградите <a href="https://www.fastcompany.com/90623897/see-34-ingenious-inspirational-winners-of-the-2021-world-changing-ideas-awards">за променящи света идеи</a> включват продукти, концепции, компании, политики и дизайни, които преследват иновациите за доброто на обществото и околната среда. Нашата система за рециклиране в магазините Looop, която превръща старите дрехи в нови, беше наградена в категорията за експериментални продукти през 2021 г.</p>
+                            <div className="blog-article-themes">
+                                <p>Наградите за променящи света идеи включват продукти, концепции, компании, политики и дизайни, които преследват иновациите за доброто на обществото и околната среда. Нашата система за рециклиране в магазините Looop, която превръща старите дрехи в нови, беше наградена в категорията за експериментални продукти през 2021 г.</p>
                             </div>
                         
-                            <h21 className="Heading-module--general__3HQET EditorialText-module--subheading__19LPD Heading-module--small__SFfSh">Looop беше инсталирана в един от магазините ни в Стокхолм.</h21>
+                            <h4 className="blog-article-title">Looop беше инсталирана в един от магазините ни в Стокхолм.</h4>
                         </div>
                     </div>
                 </article>
