ffba4872108db197a3dbe73b9da8fda02a8f55b2
Kiril Zyapkov <ziapkov@gmail.com>
2022-08-06 18:53:18 +0300

    Stiles Add to Cart button.

--- src/components/components-items/HomePageItem.jsx
+++ src/components/components-items/HomePageItem.jsx
@@ -1,44 +1,50 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import { useShoppingCart } from '../../context/ShoppingCartContext';
+import React from "react";
+import { Link } from "react-router-dom";
+import { useShoppingCart } from "../../context/ShoppingCartContext";
 
-function HomePageItem({
-    id,
-    name,
-    price,
-    image,
-    sale,
-    outOfStock,
-    newItem
-}) {
-
-const {increaseCartQuantity}  = useShoppingCart();
-
+function HomePageItem({ id, name, price, image, sale, outOfStock, newItem }) {
+  const { increaseCartQuantity } = useShoppingCart();
 
   return (
-        <li className="className1">
-            <div className="arrival-overlay">
-                                            
-                <img src={image} alt=""/>
-                {sale && <img src="images/sale.png" alt="" className="sale"/>}
-                {outOfStock && <img src="images/out.png" alt="" className="out"/>}
-                {newItem && <img src="images/new.png" alt="" className="new"/>}
-                    <div className="arrival-mask">
-                        <p onClick={() => increaseCartQuantity(id)} className="medium-button button-red add-cart">Add to Cart</p>
-                        <p className="wishlist"><i className="fa fa-heart"></i> Add to Wishlist</p>
-                        {/* <a href="#" className="compare"><i className="fa fa-retweet"></i>Add to Compare</a> */}
-                    </div>
-            </div>
-            <div className="arr-content">
-                    <Link to={`/catalog/${id}`} ><p>{name}</p></Link>
-                    <ul>
-                        <li><span className="low-price">${price}</span></li>
-                    </ul>
-                                            
-                    <div className="stars"><img src="images/stars.png" alt=""/></div>
-            </div>
-        </li>
-  )
+    <li className="className1">
+      <div className="arrival-overlay">
+        <img src={image} alt="" />
+        {sale && <img src="images/sale.png" alt="" className="sale" />}
+        {outOfStock && <img src="images/out.png" alt="" className="out" />}
+        {newItem && <img src="images/new.png" alt="" className="new" />}
+        <div className="arrival-mask">
+          <div className="buttons-group">
+            {!outOfStock && (
+              <p
+                onClick={() => increaseCartQuantity(id)}
+                className="medium-button button-red"
+              >
+                Add to Cart
+              </p>
+            )}
+            <p className="wishlist">
+              <i className="fa fa-heart"></i> Add to Wishlist
+            </p>
+            {/* <a href="#" className="compare"><i className="fa fa-retweet"></i>Add to Compare</a> */}
+          </div>
+        </div>
+      </div>
+      <div className="arr-content">
+        <Link to={`/catalog/${id}`}>
+          <p>{name}</p>
+        </Link>
+        <ul>
+          <li>
+            <span className="low-price">${price}</span>
+          </li>
+        </ul>
+
+        <div className="stars">
+          <img src="images/stars.png" alt="" />
+        </div>
+      </div>
+    </li>
+  );
 }
 
 export default HomePageItem;
