3fc9f7244270bf130aa8adf5a30f1c93f79fb8fa
Kiril Zyapkov <ziapkov@gmail.com>
2022-08-06 18:42:10 +0300

    Fetching total price from Cart.

--- src/components/HeaderMenu.jsx
+++ src/components/HeaderMenu.jsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import { useShoppingCart } from '../context/ShoppingCartContext';
+import React from "react";
+import { Link } from "react-router-dom";
+import { useShoppingCart } from "../context/ShoppingCartContext";
 
-import CartItem from './components-items/CartItem';
+import CartItem from "./components-items/CartItem";
+
+import products from "../data/products.json";
 
 function HeaderMenu() {
+  const { cartQuantity, cartItems } = useShoppingCart();
 
-    const {cartQuantity, cartItems} = useShoppingCart();
-
-    return (
-        <header className="clearfix">
-           
-            <div className="top-line">
-                <div className="nocontainer">
-                    <div className="left-line">
-                        {/* <ul className="lang clearfix">
+  return (
+    <header className="clearfix">
+      <div className="top-line">
+        <div className="nocontainer">
+          <div className="left-line">
+            {/* <ul className="lang clearfix">
                             <li><span>Language: </span> <a href="/">Eng </a>
                                 <ul className="drop-down2">
                                     <li><a href="/">Fra</a></li>
@@ -31,127 +31,189 @@ function HeaderMenu() {
                             </li>
                         </ul> */}
 
-                        <div className="mobile-a">
-                            <a href="#login-box" className="login-window"><i className="fa fa-user"></i></a>
-                            <a href="/"><i className="fa fa-heart"></i></a>
-                        </div>
-                    </div>
-                    <div className="right-line clearfix">
-                        <ul>
-                            <li><Link to="/my-account">My Account</Link></li>
-                            <li><Link to="/my-wishlist">My Wishlist</Link></li>
-                            <li><Link to="/checkout">Checkout</Link></li>
-                            <li><a href="#login-box" className="login-window">Login</a></li>
-                        </ul>
-                        <div className="mobile-version">
-                            <div className="cart-icon">
-                                <a href="/"><img src="../../images/cart-white.png" alt="" />
-                                    <span>8 Items</span></a>
-                            </div>
-                        </div>
-
-                        <div id="login-box" className="login-popup">
-                            <a href="/" className="close"><img src="../../images/delete.png" className="btn_close"
-                                title="Close Window" alt="Close" /></a>
-                            <form method="post" className="signin">
-                                <fieldset className="textbox">
-                                    <h4 className="login-title">LOGIN </h4>
-
-                                    <input placeholder="Email*" />
-                                    <input placeholder="Password*" />
-
-                                    <input type="checkbox" name="signed" value="Stayin" /> <span className="signed">Remember
-                                        Me</span>
-
-                                    <button className="submit a" type="a">Login</button>
-                                    <p>
-                                        <a className="recover" href="/">Sign up</a>
-                                        <a href="/" className="forgot2">Forgot password?</a>
-                                    </p>
-                                    <div className="clear"></div>
-
-                                    <div className="log-socials">
-                                        <h6>LOGIN WITH</h6>
-                                        <ul>
-                                            <li><a href="/"><i className="fa fa-facebook"></i></a></li>
-                                            <li><a href="/"><i className="fa fa-twitter"></i></a></li>
-                                            <li><a href="/"><i className="fa fa-google-plus"></i></a></li>
-                                        </ul>
-
-                                        <div className="clear"></div>
-                                    </div>
-                                </fieldset>
-                            </form>
-                        </div>
-
-                    </div>
-                    <div className="clear"></div>
-                </div>
+            <div className="mobile-a">
+              <a href="#login-box" className="login-window">
+                <i className="fa fa-user"></i>
+              </a>
+              <a href="/">
+                <i className="fa fa-heart"></i>
+              </a>
+            </div>
+          </div>
+          <div className="right-line clearfix">
+            <ul>
+              <li>
+                <Link to="/my-account">My Account</Link>
+              </li>
+              <li>
+                <Link to="/my-wishlist">My Wishlist</Link>
+              </li>
+              <li>
+                <Link to="/checkout">Checkout</Link>
+              </li>
+              <li>
+                <a href="#login-box" className="login-window">
+                  Login
+                </a>
+              </li>
+            </ul>
+            <div className="mobile-version">
+              <div className="cart-icon">
+                <a href="/">
+                  <img src="../../images/cart-white.png" alt="" />
+                  <span>8 Items</span>
+                </a>
+              </div>
             </div>
 
-
-            <div className="upper-header container">
-                <div className="nocontainer">
-
-                    <div className="search-input">
-                        <form>
-                            <input type="text" placeholder="Type and Search" />
-                            <input type="submit" value="" />
-                        </form>
-                    </div>
-
-                    <div className="logo">
-                        <a href="index.html"><img src="images/logo.png" alt="" /></a>
-                    </div>
-
-                    {cartQuantity !== 0 && <div className="cart">
-                        <a href="/" className="cartmain"> Cart </a>
-                        <div className="card-icon">
-                            <img src="images/cart.png" alt="" />
-                            <div className="shop-items">{cartQuantity}</div>
-                        </div>
-                        <div className="hover-cart">
-
-                            {cartItems.map(item => <CartItem key={item.id} {...item} />)}
-
-                            <div className="subtotal">
-                                Cart Subtotal: <span>$ 4 372</span>
-                            </div>
-
-                            <button className="viewcard"> View Cart</button>
-                            <button className="proceedcard"> Proceed</button>
-
-                        </div>
-                    </div>}
+            <div id="login-box" className="login-popup">
+              <a href="/" className="close">
+                <img
+                  src="../../images/delete.png"
+                  className="btn_close"
+                  title="Close Window"
+                  alt="Close"
+                />
+              </a>
+              <form method="post" className="signin">
+                <fieldset className="textbox">
+                  <h4 className="login-title">LOGIN </h4>
+                  <input placeholder="Email*" />
+                  <input placeholder="Password*" />
+                  <input type="checkbox" name="signed" value="Stayin" />{" "}
+                  <span className="signed">Remember Me</span>
+                  <button className="submit a" type="a">
+                    Login
+                  </button>
+                  <p>
+                    <a className="recover" href="/">
+                      Sign up
+                    </a>
+                    <a href="/" className="forgot2">
+                      Forgot password?
+                    </a>
+                  </p>
+                  <div className="clear"></div>
+                  <div className="log-socials">
+                    <h6>LOGIN WITH</h6>
+                    <ul>
+                      <li>
+                        <a href="/">
+                          <i className="fa fa-facebook"></i>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="/">
+                          <i className="fa fa-twitter"></i>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="/">
+                          <i className="fa fa-google-plus"></i>
+                        </a>
+                      </li>
+                    </ul>
 
                     <div className="clear"></div>
+                  </div>
+                </fieldset>
+              </form>
+            </div>
+          </div>
+          <div className="clear"></div>
+        </div>
+      </div>
 
+      <div className="upper-header container">
+        <div className="nocontainer">
+          <div className="search-input">
+            <form>
+              <input type="text" placeholder="Type and Search" />
+              <input type="submit" value="" />
+            </form>
+          </div>
+
+          <div className="logo">
+            <a href="index.html">
+              <img src="images/logo.png" alt="" />
+            </a>
+          </div>
+
+          {cartQuantity !== 0 && (
+            <div className="cart">
+              <a href="/" className="cartmain">
+                {" "}
+                Cart{" "}
+              </a>
+              <div className="card-icon">
+                <img src="images/cart.png" alt="" />
+                <div className="shop-items">{cartQuantity}</div>
+              </div>
+              <div className="hover-cart">
+                {cartItems.map((item) => (
+                  <CartItem key={item.id} {...item} products={products} />
+                ))}
+
+                <div className="subtotal">
+                  Cart Subtotal:{" "}
+                  <span>
+                    ${" "}
+                    {cartItems
+                      .map((item) => {
+                        const foundItem = products.find(
+                          (i) => i.id === item.id
+                        );
+                        if (foundItem === undefined) return;
+                        return foundItem.price * item.quantity;
+                      })
+                      .reduce((prevItem, nextItem) => prevItem + nextItem, 0)}
+                  </span>
                 </div>
 
+                <button className="viewcard"> View Cart</button>
+                <button className="proceedcard"> Proceed</button>
+              </div>
             </div>
+          )}
 
+          <div className="clear"></div>
+        </div>
+      </div>
 
-            <div className="container">
-                <div className="nocontainer bannerborder">
-
-                    <nav id="nav">
-                        <ul id="navlist" className="sf-menu clearfix">
-                            <li className="current"><Link  to="/">Home</Link></li>
-                            <li><Link to="/catalog">Shop</Link></li>
-                            <li><Link to="/women">Women</Link></li>
-                            <li><Link to="/men">Men</Link></li>
-                            <li><Link to="/accesories">Accesories</Link></li>
-                            <li><Link to="/about-us">About</Link></li>
-                            <li><Link to="/blog">Blog</Link></li>
-                            <li><Link to="/contact-us">Contact</Link></li>
-                        </ul>
-                    </nav>
-
-                </div>
-            </div>
-
-        </header>
-    )
+      <div className="container">
+        <div className="nocontainer bannerborder">
+          <nav id="nav">
+            <ul id="navlist" className="sf-menu clearfix">
+              <li className="current">
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/catalog">Shop</Link>
+              </li>
+              <li>
+                <Link to="/women">Women</Link>
+              </li>
+              <li>
+                <Link to="/men">Men</Link>
+              </li>
+              <li>
+                <Link to="/accesories">Accesories</Link>
+              </li>
+              <li>
+                <Link to="/about-us">About</Link>
+              </li>
+              <li>
+                <Link to="/blog">Blog</Link>
+              </li>
+              <li>
+                <Link to="/contact-us">Contact</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
 }
 
-export default HeaderMenu
+export default HeaderMenu;
--- src/components/components-items/CartItem.jsx
+++ src/components/components-items/CartItem.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import products from "../../data/products.json";
-
-function CartItem({ id, quantity }) {
+function CartItem({ id, quantity, products }) {
   const item = products.find((i) => i.id === id);
 
   if (item === null) return null;
 
   return (
     <div className="hover-box">
-      <Link to="/">
+      <Link to={`/catalog/${item.id}`}>
         <img src={item.image} alt="" className="left-hover" />
       </Link>
       <div className="hover-details">
@@ -19,7 +17,7 @@ function CartItem({ id, quantity }) {
         <div className="quantity">Quantity: {quantity}</div>
       </div>
 
-      <a href="/" className="right-hover">
+      <a className="right-hover">
         <img src="../../images/delete.png" alt="" />X
       </a>
 
